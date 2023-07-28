60c8faa3ab7057ddb8242f6756029807329b65e7
MartaShchur <m.matseyko199@gmail.com>
2023-07-28 13:05:55 +0200

    made 1-3

--- js/task-02.js
+++ js/task-02.js
@@ -6,3 +6,14 @@ const ingredients = [
   'Herbs',
   'Condiments',
 ];
+
+// const list = document.querySelector('#ingredients');
+
+//  ingredients.map((el) => {
+//   const li = document.createElement('li');
+//   li.textContent = el;
+//    li.classList.add('item');
+//    list.append(li);
+// });
+
+// console.log(list);
---
+++ js/task-04.js
@@ -0,0 +1,41 @@
+// Створи змінну counterValue, в якій буде зберігатися поточне значення лічильника
+//  та ініціалізуй її значенням 0.
+// Додай слухачів кліків до кнопок, всередині яких збільшуй
+// або зменшуй значення лічильника.
+// Оновлюй інтерфейс новим значенням змінної counterValue.
+
+// const counterValueElement = document.getElementById('counterValue');
+// const incrementBtn = document.querySelector('incrementBtn');
+// const decrementBtn = document.querySelector('decrementBtn');
+
+// incrementBtn.addEventListener('click', handlerIncrementCounter);
+// decrementBtn.addEventListener('click', handlerDecrementCounter);
+
+// let counterValue = 0;
+
+// function updateCounterValue() {
+//       counterValueElement.textContent = counterValue;
+// }
+    
+// function handlerIncrementCounter() {
+//     counterValue =+ 1;
+//     updateCounterValue();
+// }
+
+// function  handlerDecrementCounter() {
+//     counterValue =-1;
+//     updateCounterValue();
+// }
+
+// updateCounterValue();
+
+
+
+
+
+
+
+
+
+
+
